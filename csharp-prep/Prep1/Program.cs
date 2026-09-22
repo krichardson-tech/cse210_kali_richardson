dc9f47205b732ddc08db9ec60b0b3fb016da478e
krichardson-tech <kali.r.4115@gmail.com>
2026-09-21 21:20:37 -0600

    declaring user string

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        string firstName;
-        string lastName;
         Console.WriteLine("Please enter your first name: ");
-        firstName = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.WriteLine("Please enter your last name: ");
-        lastName = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
