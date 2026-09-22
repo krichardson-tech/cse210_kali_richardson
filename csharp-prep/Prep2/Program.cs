046f500211bc399dc134ee65675e155a93f7f7f9
krichardson-tech <kali.r.4115@gmail.com>
2026-09-21 21:44:45 -0600

    letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is you grade percentage?");
+        string number = Console.ReadLine();
+        int percent = int.Parse(number);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats! You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you failed, better luck next time.");
+        }
     }
 }
