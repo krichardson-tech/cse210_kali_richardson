d77b92aaf68fa399a545ea0f0ec21f396af11c13
krichardson-tech <kali.r.4115@gmail.com>
2026-09-24 09:28:48 -0600

    while loops

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,22 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 10;
-        int y = 30;
-        int z = 40;
-        if ((x == 10 || y == 30) && z == 30)
-        {
-            Console.WriteLine("X is 10");
-            Console.WriteLine("Y is fun");
-        }
-        else if (x == 20)
-        {
-            Console.WriteLine("X is 20");
-        }
-        else
-        {
-            Console.WriteLine("Default Output");
-        }
+        //IF, ELSE & ELSE IF
+        // int x = 10;
+        // int y = 30;
+        // int z = 40;
+        // if ((x == 10 || y == 30) && z == 30)
+        // {
+        //     Console.WriteLine("X is 10");
+        //     Console.WriteLine("Y is fun");
+        // }
+        // else if (x == 20)
+        // {
+        //     Console.WriteLine("X is 20");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Default Output");
+        // }
         
+        // WHILE LOOPS
+        bool done = false;
+        while (! done)
+        {
+            Console.Write("Are we done (y/n)? ");
+            done = Console.ReadLine() == "y";
+        }
+
+        //FOR LOOPS
+
+
+        //LISTS LIST<int> = NEW LIST<INT>()
+
+
+        //FUNCTIONS
     }
 }
